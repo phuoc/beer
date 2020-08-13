79e681761257f211145c839abd88e496b8a0623d
Phuoc Vien Ngo <phuoc_vien@hotmail.com>
2020-08-13 14:15:19 +0200

    Update beercalc.js

--- scripts/beercalc.js
+++ scripts/beercalc.js
@@ -2,24 +2,12 @@
  * Created by fokwi on 08.02.2017.
  */
 
-
-
-
 function calc() {
     var a = document.getElementById("option1");
     var b = document.getElementById("option2");
     var c = document.getElementById("option3");
     var grains = document.getElementById("textinput").value;
     var reqGrains = 5;
-
-    if(a.checked) {
-        reqGrains = 5;
-    } else if(b.checked) {
-        reqGrains = 2;
-    } else if(c.checked){
-        reqGrains = 1;
-    }
-
     var antBeer = grains/reqGrains;
     var tSugar = Math.floor(antBeer);
     var tLeavening = Math.floor(antBeer*2);
@@ -27,18 +15,30 @@ function calc() {
     var tGrains = Math.floor(grains);
     var tBeer = Math.floor(antBeer);
 
-
-    console.log("antBeer: " + grains);
-    document.getElementById("totalSugar").innerHTML = tSugar;
-    document.getElementById("totalLeavening").innerHTML = tLeavening;
-    document.getElementById("totalWater").innerHTML = tWater;
-    document.getElementById("grains").innerHTML = Math.floor(reqGrains);
-    document.getElementById("totalGrains").innerHTML = tGrains;
-    document.getElementById("totalBeer").innerHTML = tBeer;
+    if(a.checked) {
+        reqGrains = 5;
+        updateTable(reqGrains);
+    } else if(b.checked) {
+        reqGrains = 2;
+        updateTable(reqGrains);
+    } else if(c.checked){
+        reqGrains = 1;
+        updateTable(reqGrains);
+    }
 
 }
 
 function changeRecipe(i) {
-    console.log("heiheihallo");
+    console.log("changeRecipe");
     document.getElementById("grains").innerHTML = i;
+}
+
+function updateTable(r) {
+    console.log("antBeer: " + grains)
+    document.getElementById("totalSugar").innerHTML = tSugar;
+    document.getElementById("totalLeavening").innerHTML = tLeavening;
+    document.getElementById("totalWater").innerHTML = tWater;
+    document.getElementById("grains").innerHTML = Math.floor(r);
+    document.getElementById("totalGrains").innerHTML = tGrains;
+    document.getElementById("totalBeer").innerHTML = tBeer;
 }
